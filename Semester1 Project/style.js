ee22cca35eadb5b4c2551bde8c08bdb344df0361
syedhasnainabbaszaidi <syedhasnainabbaszaidi287@gmail.com>
2026-03-06 17:12:40 +0500

    hasnain commit

---
+++ Semester1 Project/style.js
@@ -0,0 +1,11 @@
+{/* <script>
+document.querySelector(".dropdown-btn").onclick=function(){
+document.querySelector(".dropdown-menu").classList.toggle("show");
+};
+
+document.querySelector(".dropdown-menu").classList.add("hide");
+</script>
+
+<style>
+.show{display:block;}
+</style> */}
