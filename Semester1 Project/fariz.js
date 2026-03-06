97e434e9211fdecf4a2524f70771a8a6de8f76b6
MuhamadFariz <fariz72010@gmail.com>
2026-03-06 16:54:20 +0500

    First commit

---
+++ Semester1 Project/fariz.js
@@ -0,0 +1,37 @@
+// Add to cart button work
+function button(){
+  alert("Product is added to cart...")
+}
+
+// Newsletter input work
+function auth(){
+  let inputnew=document.getElementById(`inpnews`).value
+// condition
+if(inputnew==""){
+alert("Please enter your email first...")
+}
+else{
+  alert(`Thank you for signing in as ${inputnew}`)
+
+}
+}
+
+// Offer card click function
+function off10(){
+  window.location.href="Fariz2.html"
+}
+
+// Buy now button work
+function buybtn(){
+  alert("Thanks for buying from us👍...")
+}
+
+// Order now button work
+function ordbtn(){
+  alert("Thanks for the order😊...")
+}
+
+// Buy of home page button
+function buybutt(){
+  alert("Thanks for buying Our featured product😁...")
+}
