5cf66cb4d9b9b24652d68dad46edca949edf54e1
MuhamadFariz <fariz72010@gmail.com>
2026-03-08 23:25:40 +0500

    Final work of Project

--- Semester1 Project/fariz.js
+++ Semester1 Project/fariz.js
@@ -4,11 +4,12 @@ function button(){
 }
 
 // Newsletter input work
+let errormess=document.getElementById("emperror")
 function auth(){
   let inputnew=document.getElementById(`inpnews`).value
 // condition
 if(inputnew==""){
-alert("Please enter your email first...")
+errormess.innerHTML="Please enter your email first..."
 }
 else{
   alert(`Thank you for signing in as ${inputnew}`)
@@ -31,7 +32,63 @@ function ordbtn(){
   alert("Thanks for the order😊...")
 }
 
-// Buy of home page button
-function buybutt(){
-  alert("Thanks for buying Our featured product😁...")
+// Buy of home page buttons
+function buybutt1(){
+  if(confirm(`ORGANIC OATS
+Pkr. 450/1kg
+
+BUY NOW 👍`)){
+  alert("Thanks for buying Our featured product😁...");
+}
+  
+}
+function buybutt2(){
+  if( confirm(`LAL-MIRCH POWDER
+Pkr. 330/100g
+
+BUY NOW 👍`)){
+  alert("Thanks for buying Our featured product😁...");
+}
+  
+}
+function buybutt3(){
+  if( confirm(`CHICK PEAS (CHANA)
+Pkr. 310/1kg
+
+BUY NOW 👍`)){
+  alert("Thanks for buying Our featured product😁...");
+}
+  
+}
+function buybutt4(){
+   if(confirm(`CHAAT MASALA
+Pkr. 280/100g
+
+BUY NOW 👍`)){
+  alert("Thanks for buying Our featured product😁...");
+}
+  
+}
+function buybutt5(){
+   if(confirm(`BASMATI RICE
+Pkr. 470/1kg
+
+BUY NOW 👍`)){
+  alert("Thanks for buying Our featured product😁...");
+}
+  
+}
+function buybutt6(){
+   if(confirm(`TURMERIC(HALDI) POWDER
+Pkr. 200/100g
+
+BUY NOW 👍`)){
+  alert("Thanks for buying Our featured product😁...");
+}
+  
+}
+
+// About page btn from Home page
+function aboutbtn(){
+  window.location.href="aboutus.html"
 }
